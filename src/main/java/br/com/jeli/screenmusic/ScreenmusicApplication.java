9f3dc0c9fa1ab7aa44c6241f4dcc5ad2bf716df3
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-06-15 17:50:09 -0300

    feat: 'Anotando com @Autowired para que o Spring instancie nossa interface'

--- src/main/java/br/com/jeli/screenmusic/ScreenmusicApplication.java
+++ src/main/java/br/com/jeli/screenmusic/ScreenmusicApplication.java
@@ -1,11 +1,16 @@
 package br.com.jeli.screenmusic;
 
+import br.com.jeli.screenmusic.main.Main;
+import br.com.jeli.screenmusic.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmusicApplication implements CommandLineRunner {
+	@Autowired
+	private ArtistRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmusicApplication.class, args);
@@ -14,5 +19,6 @@ public class ScreenmusicApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Running...");
+		new Main(this.repository).execute();
 	}
 }

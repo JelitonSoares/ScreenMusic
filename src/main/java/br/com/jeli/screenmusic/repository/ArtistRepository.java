de8e5eeb962990870a5eb7ffb1038a74da5893c6
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-06-16 10:23:13 -0300

    feat: 'Adicionando o método de findMusicByName()'

--- src/main/java/br/com/jeli/screenmusic/repository/ArtistRepository.java
+++ src/main/java/br/com/jeli/screenmusic/repository/ArtistRepository.java
@@ -1,11 +1,16 @@
 package br.com.jeli.screenmusic.repository;
 
 import br.com.jeli.screenmusic.domain.Artist;
+import br.com.jeli.screenmusic.domain.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
     Optional<Artist> findByNameContainingIgnoreCase(String name);
+    @Query("SELECT m FROM Artist a JOIN a.musics m WHERE m.singer = :artist")
+    List<Music> findMusicByArtist(Artist artist);
 }

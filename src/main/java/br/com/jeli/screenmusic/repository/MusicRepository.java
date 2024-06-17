90e7419d78a96d8ae998009973bb49b52274755f
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-06-17 11:47:49 -0300

    feat: 'Implementando o método de buscar música por um gênero'

--- src/main/java/br/com/jeli/screenmusic/repository/MusicRepository.java
+++ src/main/java/br/com/jeli/screenmusic/repository/MusicRepository.java
@@ -1,7 +1,12 @@
 package br.com.jeli.screenmusic.repository;
 
 import br.com.jeli.screenmusic.domain.Music;
+import br.com.jeli.screenmusic.domain.MusicCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    List<Music> findByGender(MusicCategory category);
 }
